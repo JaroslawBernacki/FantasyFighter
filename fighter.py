140b1bdcd40477a395fda50939a8f713e10c2465
Jarosław Bernacki <339185@uwr.edu.pl>
2023-03-23 21:13:45 +0100

    Added basic animations and sprites. Characters now move,jump,fight,flinch when hurt and die.

--- fighter.py
+++ fighter.py
@@ -1,41 +1,91 @@
 import pygame
 class Player():
-    def __init__(self,x,y):
-        self.flip=False
+    def __init__(self,gracz,x,y,flip,dane,sprajt,kroki_animacji):
+        self.gracz=gracz
+        self.size=dane[0]
+        self.image_scale=dane[1]
+        self.offset=dane[2]
+        self.flip=flip
+        self.animation_list=self.load_images(sprajt,kroki_animacji)
+        self.action=5
+        self.frame_index=0
+        self.image=self.animation_list[self.action][self.frame_index]
+        self.update_time=pygame.time.get_ticks()
         self.rect=pygame.Rect((x,y,80,160))
         self.vel_y=0
         self.skok=False
+        self.running=False
         self.typ_ataku=0
-        self.attack_cooldown=False
+        self.attack_cooldown=0
+        self.w_trakcie_ataku=False
         self.zdrowie=100
-
+        self.alive=True
+        self.trafienie=False
         
+    def load_images(self,sprajt,kroki_animacji):
+        animation_list=[]
+        for y,animation in enumerate(kroki_animacji):
+            temp_img_list=[]
+            for i in range(animation):
+                temp_img=sprajt[y].subsurface(i*self.size,0,self.size,self.size)
+
+                temp_img_list.append(pygame.transform.scale(temp_img,(self.size*self.image_scale,self.size*self.image_scale)))
+            animation_list.append(temp_img_list)
+        return animation_list
     def move(self,szerokosc_okna,wysokosc_okna,surface,target):
         Speed = 10
         Grawitacja=2
         dx = 0
         dy = 0
+        self.running=False
+        self.typ_ataku=0
         #wychwyc wcisniety klawisz
         key=pygame.key.get_pressed()
         #tylko jesli nie atakuje
-        if self.attack_cooldown==False:
-        #atak
-            if key[pygame.K_r] or key[pygame.K_t]:
-                self.attack(surface,target)
-                if key[pygame.K_r]:
-                    self.typ_ataku=1
-                if key[pygame.K_t]:
-                    self.typ_ataku=2
+        if self.w_trakcie_ataku==False and self.alive==True:
+            #sprawdz ktory gracz
+            if self.gracz==1:
+                #atak
+                if key[pygame.K_r] or key[pygame.K_t]:
+                    self.attack(surface,target)
+                    if key[pygame.K_r]:
+                        self.typ_ataku=1
+                    if key[pygame.K_t]:
+                        self.typ_ataku=2
 
-            #ruch graczy
-            if key[pygame.K_a]:
-                dx=-Speed
-            if key[pygame.K_d]:
-                dx=Speed
-            #skok
-            if key[pygame.K_w] and self.skok==False:
-                self.skok=True
-                self.vel_y=-30
+                #ruch graczy
+                if key[pygame.K_a]:
+                    dx=-Speed
+                    self.running=True
+                if key[pygame.K_d]:
+                    dx=Speed
+                    self.running=True
+                #skok
+                if key[pygame.K_w] and self.skok==False:
+                    self.skok=True
+                    self.vel_y=-30
+
+            elif self.gracz==2:
+                #atak
+                
+                if key[pygame.K_KP1] or key[pygame.K_KP2]:
+                    self.attack(surface,target)
+                    if key[pygame.K_KP1]:
+                        self.typ_ataku=1
+                    if key[pygame.K_KP2]:
+                        self.typ_ataku=2
+
+                #ruch graczy
+                if key[pygame.K_LEFT]:
+                    dx=-Speed
+                    self.running=True
+                if key[pygame.K_RIGHT]:
+                    dx=Speed
+                    self.running=True
+                #skok
+                if key[pygame.K_UP] and self.skok==False:
+                    self.skok=True
+                    self.vel_y=-30
         self.vel_y+=Grawitacja
         dy += self.vel_y
         #pozycja gracza
@@ -54,13 +104,74 @@ class Player():
             self.flip=True
         self.rect.x+=dx
         self.rect.y+=dy
+        #cooldown ataku
+        if self.attack_cooldown>0:
+            self.attack_cooldown-=1
+
+    def update(self):
+        #Jezeli zdrowie jest rowne lub mniejsze 0 gracz umiera
+        if self.zdrowie<=0:
+            self.zdrowie=0
+            self.alive=False
+            self.update_action(3)
+        #wybor animacji
+        elif self.trafienie==True:
+            self.update_action(4)
+        elif self.w_trakcie_ataku==True:
+            if self.typ_ataku==1:
+                self.update_action(0)
+            elif self.typ_ataku==2:
+                self.update_action(1)
+        elif self.skok==True:
+            self.update_action(6)
+        elif self.running==True:
+            self.update_action(8)
+        else:
+            self.update_action(5)
+
+        animation_cooldown=200
+        #wyswietl wybrana animacje
+        self.image=self.animation_list[self.action][self.frame_index]
+        #zmiana klatki animacji
+        if pygame.time.get_ticks()-self.update_time>animation_cooldown:
+            self.frame_index += 1
+            self.update_time=pygame.time.get_ticks()
+        #sprawdz czy animacja sie skonczyla
+        if self.frame_index>=len(self.animation_list[self.action]):
+            #jezeli gracz umarl zakoncz animacje na ostatniej klatce
+            if self.alive==False:
+                self.frame_index=len(self.animation_list[self.action])-1
+            else:
+                self.frame_index=0
+                #obsluga cooldowna ataku
+                if self.action==0 or self.action==1 or self.action==2:
+                    self.w_trakcie_ataku=False
+                    self.attack_cooldown=50
+                if self.action==4:
+                    self.trafienie=False
+                    #jezeli gracz jest trafiony w trakcie animacji animacja jest przerwana
+                    self.w_trakcie_ataku=False
+                    self.attack_cooldown=50
+        
+
     def attack(self,surface,target):
-        self.attack_cooldown=True
-        attacking_rect=pygame.Rect(self.rect.centerx-2*self.rect.width*self.flip,self.rect.y,2 * self.rect.width,self.rect.height)
-        if attacking_rect.colliderect(target.rect):
-            print("hit")
-            target.zdrowie-=10
-        pygame.draw.rect(surface,(0,255,0),attacking_rect)
+        if self.attack_cooldown==0:
+            self.w_trakcie_ataku=True
+            attacking_rect=pygame.Rect(self.rect.centerx-2*self.rect.width*self.flip,self.rect.y,2 * self.rect.width,self.rect.height)
+            if attacking_rect.colliderect(target.rect):
+                print("hit")
+                target.zdrowie-=10
+                target.trafienie=True
+            pygame.draw.rect(surface,(0,255,0),attacking_rect)
+
+    def update_action(self,new_action):
+        if new_action != self.action:
+            self.action=new_action
+            self.frame_index=0
+            self.update_time=pygame.time.get_ticks()
+
     def draw(self,surface):
+        img=pygame.transform.flip(self.image,self.flip,False)
         pygame.draw.rect(surface,(255,0,0),self.rect)
+        surface.blit(img,(self.rect.x-self.offset[0]*self.image_scale,self.rect.y-self.offset[1]*self.image_scale))
 
--- main.py
+++ main.py
@@ -11,13 +11,36 @@ clock=pygame.time.Clock()
 FPS=60
 #Deklaracja tekstur
 tlo=pygame.image.load("assets/images/background/tlo_tiles.png").convert_alpha()
+gracz1tekstury=[]
+gracz2tekstury=[]
+lista_sprajtow=["Attack_1.png","Attack_2.png","Attack_3.png","Dead.png","Hurt.png","Idle.png","Jump.png","Protect.png","Run.png","Walk.png"]
+for i in lista_sprajtow:
+    sciezka="assets/images/characters/sprites/Samurai_warrior/"+i
+    gracz1tekstura=pygame.image.load(sciezka).convert_alpha()
+    gracz1tekstury.append(gracz1tekstura)
+for i in lista_sprajtow:
+    sciezka="assets/images/characters/sprites/Samurai_warrior/"+i
+    gracz2tekstura=pygame.image.load(sciezka).convert_alpha()
+    gracz2tekstury.append(gracz2tekstura)
+#gracz2tekstura=pygame.image.load("assets/images/characters/sprites/Skeleton_warrior/Walk.png").convert_alpha()
+#dane graczy
+player1size=128
+player1scale=4
+player1offset=[72,56]
+player2size=128
+player2scale=4
+player2offset=[72,56]
+player1data=[player1size,player1scale,player1offset]
+player2data=[player2size,player2scale,player2offset]
+gracz1_kroki_animacji=[4,5,4,6,2,5,7,2,8,9]
+gracz2_kroki_animacji=[4,5,4,6,2,5,7,2,8,9]
 #Funkcje zwiazane z rysowaniem tekstur
 def rysuj_tlo():
     tlo_przeskalowane=pygame.transform.scale(tlo,(Szerokosc_okna,Wysokosc_okna))
     okno.blit(tlo_przeskalowane,(0,0))
 #Funkcje zwiazane z silnikiem gry
-fighter_1=Player(200,310)
-fighter_2=Player(700,310)
+fighter_1=Player(1,200,310,False,player1data,gracz1tekstury,gracz1_kroki_animacji)
+fighter_2=Player(2,700,310,True,player2data,gracz2tekstury,gracz2_kroki_animacji)
 #Rysowanie paskow zdrowia
 def rysuj_pasek_zdrowia(zdrowie,x,y):
     ratio= zdrowie/100
@@ -33,7 +56,9 @@ while run:
     rysuj_pasek_zdrowia(fighter_1.zdrowie,20,20)
     rysuj_pasek_zdrowia(fighter_2.zdrowie,620,20)
     fighter_1.move(Szerokosc_okna,Wysokosc_okna,okno,fighter_2)
-    #fighter_2.move(Szerokosc_okna,Wysokosc_okna,okno,fighter_1)
+    fighter_2.move(Szerokosc_okna,Wysokosc_okna,okno,fighter_1)
+    fighter_1.update()
+    fighter_2.update()
     fighter_1.draw(okno)
     fighter_2.draw(okno)
     for event in pygame.event.get():
